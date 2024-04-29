fb152a789a3852bf5f3286b2fc8407565d072160
UnodBukod <yanyanbucod@gmail.com>
2024-04-29 20:05:05 +0800

    Babye Image

--- src/app/pages/features/components/firstsectioncard.tsx
+++ src/app/pages/features/components/firstsectioncard.tsx
@@ -11,13 +11,6 @@ const firstCard = ({ title, iconSrc }: cardprops) => {
     <div className="relative flex justify-center items-center">
       <div className=" mx-[1px] sm:mx-[10px] md:mx-[15px] xl:mx-[40px] w-[70px] h-[70px] sm:w-[90px] sm:h-[90px] md:w-[110px] md:h-[110px] xl:w-[190px] xl:h-[250px] xl:mt-20 flex-col items-center justify-center">
         <div className=" mb-4 ">
-          <Image
-            src="/fourthsec.svg"
-            alt="Image Description"
-            width={500}
-            height={300}
-            className="w-[50px] h-[50px] sm:w-[35px] sm:h-[35px] md:w-[45px] md:h-[45px] xl:w-[80px] xl:h-[80px] mx-auto "
-          />
           {iconSrc && (
           <div className="mb-4">
             <Image
